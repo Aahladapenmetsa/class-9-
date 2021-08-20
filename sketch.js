b409947cb16382693c42deb17d23462e4eea1e35
Aahladapenmetsa <89256568+Aahladapenmetsa@users.noreply.github.com>
2021-08-20 17:47:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+function setup() {
+  createCanvas(400,400);
+  var box = createSprite(200,200,30,30)
+}
+
+function draw() 
+{
+  background("white");
+  if(keyIsDown(RIGHT_ARROW)){
+box.position.x = box.position.x+5
+  }
+
+  if(keyIsDown(LEFT_ARROW)){
+    box.position.x = box.position.x-2;
+  }
+drawSprites();
+}
+
+
+
+
